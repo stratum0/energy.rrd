d389d320550fde5618bf03de13301eab0d08645a
Roland Hieber <rohieb@rohieb.name>
2016-09-25 04:41:33 +0200

    build.sh: warn if errors happened and target didn't get rendered

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 do_update=1
 do_rrdupdate=
 WEBROOT=/var/www/html/energy
+stamp=`mktemp /tmp/energy.rrd.stamp.XXXXX`
 
 if [ "$1" = "-n" -o "$1" = "--no-update" ]; then
   do_update=
@@ -15,7 +16,9 @@ fi
 
 if [ ! -r "data.csv" -o -n "$do_update" ]; then
   echo 'Fetching data, this may take a while... (sorry, chrissi^!)'
-  wget 'http://shiny.tinyhost.de/php/energy.php?id[]=1' -O data.csv
+  if ! wget 'http://shiny.tinyhost.de/php/energy.php?id[]=1' -O data.csv; then
+    exit 1
+  fi
 fi
 
 if [ -n "$do_rrdupdate" -o "data.csv" -nt "energy.rrd" ]; then
@@ -26,3 +29,14 @@ fi
 ./rrdgraph.sh
 
 cp energy-60d.png energy-360d.png $WEBROOT
+
+if [ $stamp -nt $WEBROOT/energy-360d.png ]; then
+  echo
+  echo "Oops. Something went wrong, the stamp file is newer than the target."
+  rm -r $stamp
+  return 1
+fi
+
+rm -r $stamp
+
+# vim: set sw=2 ts=2 et:
